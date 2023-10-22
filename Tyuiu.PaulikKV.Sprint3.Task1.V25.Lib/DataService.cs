a3f4ca4577e3157bc4cc8f07df77658b6e1630d2
paulikkirill@gmail.com <paulikkirill@gmail.com>
2023-10-22 18:15:43 +0500

    Task1

---
+++ Tyuiu.PaulikKV.Sprint3.Task1.V25.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.PaulikKV.Sprint3.Task1.V25.Lib
+{
+    public class DataService : ISprint3Task1V25
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double MultiplySeries = 1;
+            while (startValue <= stopValue)
+            {
+                MultiplySeries = MultiplySeries * ((Math.Pow(value, startValue) - (1 / 4)) * Math.Cos(5));
+                startValue++;
+            }
+            return Math.Round(MultiplySeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.PaulikKV.Sprint3.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PaulikKV.Sprint3.Task1.V25.Lib;
+
+namespace Tyuiu.PaulikKV.Sprint3.Task1.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 2;
+            int startValue = 1;
+            int stopValue = 6;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 1092.543;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

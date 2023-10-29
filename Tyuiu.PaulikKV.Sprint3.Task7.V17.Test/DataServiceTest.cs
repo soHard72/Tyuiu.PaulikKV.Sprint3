95e33e7d3c13e9f28597d63284fec7706470e452
paulikkirill@gmail.com <paulikkirill@gmail.com>
2023-10-29 15:48:49 +0500

    Task7

---
+++ Tyuiu.PaulikKV.Sprint3.Task7.V17.Test/DataServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PaulikKV.Sprint3.Task7.V17.Lib;
+
+namespace Tyuiu.PaulikKV.Sprint3.Task7.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = 0.96;
+            valueWaitArray[1] = 0.96;
+            valueWaitArray[2] = 0.96;
+            valueWaitArray[3] = 0.96;
+            valueWaitArray[4] = 0.96;
+            valueWaitArray[5] = 0.96;
+            valueWaitArray[6] = 0.96;
+            valueWaitArray[7] = 0.96;
+            valueWaitArray[8] = 0.96;
+            valueWaitArray[9] = 0.96;
+            valueWaitArray[10] = 0.96;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+
+        }
+    }
+}
---
+++ Tyuiu.PaulikKV.Sprint3.Task7.V17/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PaulikKV.Sprint3.Task7.V17.Lib;
+
+namespace Tyuiu.PaulikKV.Sprint3.Task7.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Паулик К.В. | АСОиУб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры  C#                               *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Паулик Кирилл Владимирович | АСОиУб-23-3                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ: Написать программу, которая выводит таблицу значений функции   *");
+            Console.WriteLine("* f(x)= (sin(x)/x+1,7) - cos(x) * 4x - 6 на заданном диапазоне [-5;5]     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine(" Начало отрезка = " + startValue);
+            Console.WriteLine(" Конец отрезка = " + stopValue);
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("+-----------+-----------+");
+            Console.WriteLine("|     X     |    f(x)   |");
+            Console.WriteLine("+-----------+-----------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}      |  {1,5:f2}    |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+-----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+}
